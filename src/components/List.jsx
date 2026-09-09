acdac5e0ab9469fd14fabd837cc8b456f1244097
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-09-09 09:08:28 +0200

    Refactor App component structure by moving Header, List, and Footer to components directory and removing unused code

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+function List() {
+    return (
+        <section>
+            <h2>Att gora</h2>
+            <ul>
+                <li>Lara mig komponenter</li>
+                <li>Bygga med JSX</li>
+                <li>Importera i App</li>
+            </ul>
+        </section>
+    )
+}
+
+export default List
